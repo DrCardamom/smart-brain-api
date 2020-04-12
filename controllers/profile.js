367a5f6dc817217d2a9e0fc2fc8e4f8a107bfb02
DrCardamom <erkshimba@gmail.com>
2020-04-12 11:17:12 -0400

    first commit after moving home dir

---
+++ controllers/profile.js
@@ -0,0 +1,21 @@
+const handleProfile = (req, res, db) => {
+   const { id } = req.params;
+   let found = false;
+   
+   // memo to self; ({id}) means ({id: id}});
+   db.select('*').from('users').where({id} )
+   .then(user => {
+       console.log(user);      
+
+       if(user.length) {
+           res.json(user[0]);
+       } else {
+           res.status(400).json('Not Found');
+       }
+   })
+   .catch(err => res.status(400).json('Error Geeting User'));
+}
+
+module.exports = {
+   handleProfile: handleProfile
+}
