9d0ceef34fbcc9bf90019f140565c4210cf6d321
DrCardamom <erkshimba@gmail.com>
2020-04-13 12:46:59 -0400

    updated database adress

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-polished-48922',
         user: 'shimba',
         password: 'shimba',
         database: 'smart-brain'
